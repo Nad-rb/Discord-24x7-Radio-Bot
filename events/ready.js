e0ec1a249a814737b0f7b5e74a77bc9c2e8982f8
Doraemon <71218761+Nad-rb@users.noreply.github.com>
2021-02-21 09:27:12 +0800

    Update ready.js
    Config

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("${client.user.prefix}",{ type: 'STREAMING'});
+  await client.user.setActivity("${client.config.prefix}",{ type: 'STREAMING'});
 };
